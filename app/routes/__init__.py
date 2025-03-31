02987aa385d313b0dfa61b0a8c1a631a3fb86b45
Pe-te-r <mburupeter8526@gmail.com>
2025-03-31 21:29:18 +0300

    ai added

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -2,10 +2,12 @@ def register_resource(api):
     from app.routes.user import UsersResource,UserResource
     from app.routes.code import CodesResource
     from app.routes.auth import Register,Login
+    from app.routes.ai import Ai
     
     api.add_resource(Register,'/register')
     api.add_resource(Login,'/login')
     api.add_resource(UsersResource,'/users')
     api.add_resource(UserResource,'/users/<string:id>')
+    api.add_resource(Ai,'/ai')
     
     api.add_resource(CodesResource,'/codes','/codes/<string:id>')
---
+++ app/routes/ai.py
@@ -0,0 +1,31 @@
+from flask_restful import Resource
+
+
+
+from google import genai
+
+def askQuestion(question):
+
+    client = genai.Client(api_key='AIzaSyD5UxMPYl_DLD9D7py6ktK4zGbvnKBvZFo')
+
+    response = client.models.generate_content(
+        model="gemini-2.0-flash",
+        contents=["How does AI work?"])
+    return response.text
+
+
+class Ai(Resource):
+    def post(self):
+        data=request.get_json()
+        if not data:
+            return {"status": "error", "message": "No data provided"}, 400 
+        required_field=['query']
+        is_valid,missing,correct=validate_data(data,required_field,optional='user_id')
+
+        if not is_valid:
+            return {'status':'error','message':'required field missing','error':{'field':[miss for miss in missing]}},400
+    
+
+    def get(self):
+        result=askQuestion("How does AI work?")
+        return result
